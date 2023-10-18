587af3aa2ad16933ae190ade1dd69ceb836f738a
Shyn <shyndang@aimy.io>
2023-10-18 11:38:53 +0700

    update loading

--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -1,10 +1,8 @@
 import { Loading } from "@/components/loading";
-import useChatStore from "@/stores/chat";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const LoadingBoundary = ({ children }: { children: React.ReactNode }) => {
-  const { rooms, selectedRoom } = useChatStore();
-  const [loading, setLoading] = useState(false);
+  const [loading] = useState(false);
 
   if (loading) {
     return <Loading />;
